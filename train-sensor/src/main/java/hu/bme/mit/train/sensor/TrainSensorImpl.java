4e5737626984152fb59437f37df43e0b1a0c9bdb
barabali <balintbarath@gmail.com>
2017-03-13 06:58:12 -0700

    Timer added to sensor

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.sensor;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
@@ -9,10 +12,19 @@ public class TrainSensorImpl implements TrainSensor {
 	private TrainController controller;
 	private TrainUser user;
 	private int speedLimit = 5;
+	Timer timer = new Timer();
+
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
 		this.user = user;
+		
+		timer.scheduleAtFixedRate(new TimerTask() {
+			  @Override
+			  public void run() {
+				  controller.followSpeed();
+			  }
+			},0, 1000);
 	}
 
 	@Override
