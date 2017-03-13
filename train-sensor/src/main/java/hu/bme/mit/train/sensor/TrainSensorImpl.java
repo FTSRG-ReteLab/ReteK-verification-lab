f5814b61b311f383aad48083333343281f24cd64
barabali <balintbarath@gmail.com>
2017-03-13 07:53:34 -0700

    Hiba ujra jav

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -8,8 +8,18 @@ public class TrainSensorImpl implements TrainSensor {
 	private TrainController controller;
 	private int speedLimit = 5;
 
+	Timer timer = new Timer();
+
+
 	public TrainSensorImpl(TrainController controller) {
 		this.controller = controller;
+
+		timer.scheduleAtFixedRate(new TimerTask() {
+			  @Override
+			  public void run() {
+				  controller.followSpeed();
+			  }
+			},0, 1000);
 	}
 
 	@Override
